53b664f9e9df14844e091dbdb6d04b52f3710b1f
WTYLER9 <wtyler9889@gmail.com>
2024-04-28 01:04:47 -0400

    Fruit Sales with CSV I forgot before

--- fruitsales.py
+++ fruitsales.py
@@ -1,5 +1,6 @@
 import pandas as pd
 
-pd.DataFrame({"Apples":[35,41],"Bananas":[21,34]},  index = ["2017 Sales","2018 Sales"])
+fruits_sales = pd.DataFrame({"Apples":[35,41],"Bananas":[21,34]},  index = ["2017 Sales","2018 Sales"])
+fruits_sales.to_csv("fruit.csv", encoding="utf-8")
 
 
