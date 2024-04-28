75f8d61176c9626f81e96d74ac35c1c3e236ba91
WTYLER9 <wtyler9889@gmail.com>
2024-04-28 00:59:20 -0400

    Fruit Sales Correction

--- fruitsales.py
+++ fruitsales.py
@@ -1,5 +1,5 @@
 import pandas as pd
 
-x = pd.DataFrame({"Apples":[35,41],"Bananas":[21,34]},  index = ["2017 Sales","2018 Sales"])
-print(x)
+pd.DataFrame({"Apples":[35,41],"Bananas":[21,34]},  index = ["2017 Sales","2018 Sales"])
+
 
